dd2f88a40f9d03f0ba7a3531dcc56c1564baf46e
Aurore Chevin <chevinaurore@gmail.com>
2023-09-26 06:21:24 +0200

    first commit

---
+++ src/fr/fms/Main.java
@@ -0,0 +1,14 @@
+package fr.fms;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// A. Volet initial :
+        // 1. Demander à l'utilisateur un nb entier strictement positif
+        // 2. Cas 1 : nb pair => /2 ou cas 2 : nb impair => *3+1 à appliquer
+        // 3. Exécuter la condition tant que le nombre n'est pas = 1 et donc afficher nb après chaque opération et n : tps de vol du nb
+    // B. Volet optionnel :
+        // 1. Calculer et afficher m : tps de vol en altitude, plus petit m pour lequel m+1 correspond au tps de vol d'un nb inférieur ou égal au nb de départ
+        // 2. Calculer et afficher altitude maxi: nb le plus haut atteint avec les calculs
+    }
+}
